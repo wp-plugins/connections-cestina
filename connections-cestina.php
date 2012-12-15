eb7ff53891b0d541d3464820dc2ab309a0c78bb2
expres-web <expres-web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-15 03:55:09 +0000

    Nová verze 1.1
    git-svn-id: https://plugins.svn.wordpress.org/connections-cestina/tags/1.1@639537 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ connections-cestina.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Connections čeština
+Plugin URI: http://wpguru.eu
+Description: Přeloží plugin Connections.
+Version: 1.1
+Author: Expres-Web.cz
+Author URI: http://www.expres-web.cz
+Text Domain: cpc
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Maybe load the Shortcodes Ultimate překlad.
+ *
+ * @since 1.1
+ */
+function cpc_load_textdomain() {
+	$mofile = WP_PLUGIN_DIR . '/connections-cestina/jazyky/' . apply_filters( 'connections_locale', get_locale() ) . '.mo';
+
+	if ( file_exists( $mofile ) )
+		return load_textdomain( 'connections', $mofile );
+}
+add_action( 'wp_loaded', 'cpc_load_textdomain' );
---
+++ readme.txt
@@ -0,0 +1,46 @@
+﻿=== Connections čeština ===
+Contributors: expres-web
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7F53XKXAB2HSG
+Tags: Connections, překlad, Connections čeština
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Přeloží plugin [Connections](http://wordpress.org/extend/plugins/connections/).
+
+== Installation ==
+
+Instalace je velmi jednoduchá
+
+1. Nainstalujte plugin přes administraci v Pluginy.
+1. Activujte plugin.
+
+== Screenshots ==
+Již brzy
+
+== Frequently Asked Questions ==
+
+Našel jsem chybu, co mám dělat?
+
+Pokud naleznete chybu, zdělte nám to prosím v našem fóru [WpGuru.eu](http://wpguru.eu/forum/?mingleforumaction=viewforum&f=12.0)
+
+== Changelog ==
+
+= 1.1 = 
+Testováno na WordPress v 3.5
+
+= 1.0 = 
+Uvolnění pluginu.
+
+== Upgrade Notice ==
+
+
+= 1.1 = 
+Testováno na WordPress v 3.5
+
+= 1.0 =
+Spustění a zveřejnění pluginu.
